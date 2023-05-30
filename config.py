ab4e132adac9d8f69e2c3614599990316ad4c498
Arbi <134800339+arbisahraoui@users.noreply.github.com>
2023-05-30 04:53:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 
-
 config = {
-    'images_path': './dataset/Flicker8k_images/',  # Images folder path
+    'images_path': './dataset/Flicker8k_images/',                            # Images folder path
     'captions_path': './dataset/Flickr8k_text/Flickr8k.token.txt',           # Captions path (tokens for train/val/test)
 	'train_data_path': './dataset/Flickr8k_text/Flickr_8k.trainImages.txt',  # Path for train split
 	'val_data_path': './dataset/Flickr8k_text/Flickr_8k.devImages.txt',      # Path for val split
     'test_data_path': './dataset/Flickr8k_text/Flickr_8k.testImages.txt',    # Path for test split 
+    'cleaned_captions_path': './model_data/captions.txt',                     # Cleaned captions path
+    'images_features': './model_data/features.pkl'
 }
